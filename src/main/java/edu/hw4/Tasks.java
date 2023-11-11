16848c7c10ba82469a8c7c1fbb42fb5b27219474
NurtilekMamashakirov <nurtilekmamashakirov@gmail.com>
2023-11-11 19:25:33 +0300

    style fixing

--- src/main/java/edu/hw4/Tasks.java
+++ src/main/java/edu/hw4/Tasks.java
@@ -1,15 +1,12 @@
 package edu.hw4;
 
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Tasks {
 
