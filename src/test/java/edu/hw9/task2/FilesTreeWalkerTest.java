b7c3b8d33deb7376cf855e5bcd0741eb0864ee5e
NurtilekMamashakirov <nurtilekmamashakirov@gmail.com>
2023-12-09 18:17:32 +0300

    rabota s failami fixed epta

--- src/test/java/edu/hw9/task2/FilesTreeWalkerTest.java
+++ src/test/java/edu/hw9/task2/FilesTreeWalkerTest.java
@@ -9,27 +9,27 @@ public class FilesTreeWalkerTest {
 
     @Test
     void findDirectoriesWithMore1000FilesTest() {
-        Path path = Path.of("/Users/nurtilekm/IdeaProjects/java-course-2023");
+        Path path = Path.of("src");
         List<Path> expected =
-            List.of(Path.of("/Users/nurtilekm/IdeaProjects/java-course-2023/src/test/resources/1000 файлов"));
+            List.of(Path.of("src/test/resources/1000 файлов"));
         List<Path> actual = FilesTreeWalker.findDirectoriesWithMore1000Files(path);
         assertThat(actual).containsAll(expected);
     }
 
     @Test
     void findByFileSizeTest() {
-        Path path = Path.of("/Users/nurtilekm/IdeaProjects/java-course-2023");
-        List<Path> expected = List.of(Path.of("/Users/nurtilekm/IdeaProjects/java-course-2023/.git/COMMIT_EDITMSG"));
+        Path path = Path.of("");
+        List<Path> expected = List.of(Path.of(".git/COMMIT_EDITMSG"));
         List<Path> actual = FilesTreeWalker.findByFileSize(path, 1L, 15L);
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     void findByFileExtensionTest() {
-        Path path = Path.of("/Users/nurtilekm/IdeaProjects/java-course-2023");
+        Path path = Path.of("");
         assertThat(FilesTreeWalker.findByFileExtension(path, ".xml")).contains(Path.of(
-            "/Users/nurtilekm/IdeaProjects/java-course-2023/checkstyle.xml"),
-            Path.of("/Users/nurtilekm/IdeaProjects/java-course-2023/pom.xml"));
+            "checkstyle.xml"),
+            Path.of("pom.xml"));
     }
 
 }
